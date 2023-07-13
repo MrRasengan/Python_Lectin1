f37be747ea6bed03e4439c78043c2b78b9136a87
Konstantin <kostik-kostik-12@mail.ru>
2023-07-13 23:48:51 +0300

    Get pull

---
+++ main.py
@@ -0,0 +1,25 @@
+# print()             -     вывод на экран 
+# print(f" {}  {}")   -     интрополяция
+# input()             -     ввод данных в строку
+# int(input())        -     ввод данных в число
+# print(type())       -     определиние типа данных
+# print(round())      -     определить остаток от деления
+# range()             -     значения из диапозон с шагом 1              
+# print(len())        -     проверка колличества эллементов
+# print(.lower())     -     перевод в нижний регистр
+# print(.upper())     -     перевод в верхний регистр
+# print(.replase())   -     помеять сочетание символов
+
+
+
+
+"""
+iter = 2
+iter += 3  # iter = iter + 3
+iter -= 4  # iter = iter - 4
+iter *= 5  # iter = iter * 5
+iter /= 5  # iter = iter / 5
+iter //= 5 # iter = iter // 5
+iter %= 5  # iter = iter % 5
+iter **= 5 # iter = iter ** 5
+"""
